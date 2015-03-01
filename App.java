e9b1791856bd97bfff29fc1c2f45359e5fc3726c
James Vansteenkiste <me@jamesvan.io>
2015-03-01 16:40:41 -0500

    Updated AppWindow
    Added and Updated AppWindow.

---
+++ App.java
@@ -0,0 +1,12 @@
+import java.awt.EventQueue;
+
+public class App {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				AppWindow window = new AppWindow();
+				window.setVisible(true);
+			}
+		}); }
+}
