9450d393cfae36e110360de18612bfbf2569f657
Jeremy Alcorta <jalcorta@uwo.ca>
2015-03-29 13:07:14 -0400

    Map class  (Curtis to document)

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -8,21 +8,28 @@ import java.net.URLEncoder;
 
 import javax.swing.ImageIcon;
 
+/**
+ * A class to get an image icon of the current weather location
+ * @author	Group10 (James V, Li G, Curtis V, Alec W, Jeremy A)
+ * @version	1.0
+ * @since 	March 23, 2015
+ */
 public class Map {
 	private ImageIcon map;
 	
 	/**
-	 * A class to get an image icon of the current weather location
-	 * @author	Group10 (James V, Li G, Curtis V, Alec W, Jeremy A)
-	 * @version	1.0
-	 * @since 	March 23, 2015
+	 * Constructor method.
+	 * 
+	 * @param latitude
+	 * @param longtitude
+	 * @param icon
 	 */
 	public Map(String latitude, String longtitude,String icon ){
 
 	        try {
 	        	String imageUrl = "https://maps.googleapis.com/maps/api/staticmap?center="+URLEncoder.encode(latitude, "UTF-8")+","+URLEncoder.encode(longtitude, "UTF-8")+"&maptype=terrain&zoom=10&size=355x249&markers=icon:http://openweathermap.org/img/w/"+URLEncoder.encode(icon, "UTF-8")+".png|"+URLEncoder.encode(latitude, "UTF-8")+","+URLEncoder.encode(longtitude, "UTF-8");
 	            String destinationFile = "image.jpg";
-	            System.out.println(imageUrl);
+//	            System.out.println(imageUrl);
 	            URL url = new URL(imageUrl);
 	            java.io.InputStream is = url.openStream();
 	            OutputStream os = new FileOutputStream(destinationFile);
